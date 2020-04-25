22383fb345969f9b140d09ce8aaa5de762a68b9f
Claudiu Nicola <claudiunicola96@gmail.com>
2020-04-25 19:42:37 +0300

    added firstCases&situationNow endpoints

--- backend/rest-api/handler.js
+++ backend/rest-api/handler.js
@@ -6,13 +6,17 @@ const base = new Airtable({ apiKey: process.env.airtableApiKey }).base(
   process.env.airtableId
 );
 const tableName = "responses";
+const openData = "Imported table";
+
+const errBody = (err) => {
+  return {
+    statusCode: err.statusCode,
+    body: JSON.stringify(err),
+  };
+};
 
 module.exports.create = async (event) => {
-  const data = JSON.parse(event.body);
-
   const response = {
-    state: data.state,
-    area: data.area,
     createdAt: new Date().getTime(),
   };
 
@@ -23,25 +27,75 @@ module.exports.create = async (event) => {
       body: JSON.stringify({ reference: result.id }),
     };
   } catch (err) {
-    return {
-      statusCode: err.statusCode,
-      body: JSON.stringify(err),
-    };
+    return errBody(err);
   }
 };
 
 module.exports.add = async (event) => {
-  const response = JSON.parse(event.body);
-
-  const reference = response.reference;
-  delete response.reference;
+  const body = JSON.parse(event.body);
 
   try {
-    await base(tableName).update(reference, response);
-  } catch (err) {
+    const result = await base(tableName).update(body.reference, body.data);
     return {
-      statusCode: err.statusCode,
-      body: JSON.stringify(err),
+      statusCode: 200,
+      body: JSON.stringify({ reference: result.id }),
     };
+  } catch (err) {
+    return errBody(err);
+  }
+};
+
+module.exports.firstCases = async (event) => {
+  const country = event.queryStringParameters.country;
+
+  try {
+    const results = await base(openData)
+      .select({
+        maxRecords: 1,
+        view: "Grid view",
+        filterByFormula: `AND({location} = '${country}', {total_cases} > 0)`,
+      })
+      .firstPage();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        date: results[0].get("date"),
+        no_cases: results[0].get("total_cases"),
+      }),
+    };
+  } catch (err) {
+    return errBody(err);
+  }
+};
+
+module.exports.situationNow = async (event) => {
+  const country = event.queryStringParameters.country;
+  let selectOptions = {
+    maxRecords: 1,
+    view: "Grid view",
+    sort: [{ field: "date", direction: "desc" }],
+    filterByFormula: `AND({location} = '${country}', {date} = '${new Date().toJSON().slice(0,10).replace(/-/g,'-')}')`,
+  };
+
+  try {
+    let results = await base(openData).select(selectOptions).firstPage();
+
+    // search for yestarday if no results for today
+    if (results.length == 0) {
+      selectOptions.filterByFormula = `AND({location} = '${country}', {date} = '${new Date(Date.now() - 86400000).toJSON().slice(0,10).replace(/-/g,'-')}')`
+    }
+    results = await base(openData).select(selectOptions).firstPage();
+
+    console.log(results);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        date: results[0].get("date"),
+        no_cases: results[0].get("total_cases"),
+      }),
+    };
+  } catch (err) {
+    return errBody(err);
   }
 };
